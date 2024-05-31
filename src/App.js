e299746f59c1ff77743aafb46f62763747360258
Faceless1998 <81959592+Faceless1998@users.noreply.github.com>
2024-05-31 17:46:38 +0400

    davaleba

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home/Home";
 import { About } from "./pages/about/About";
-import { Game } from "./pages/games/Game";
+import { Book } from "./pages/book/Book";
+
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/games" element={<Game />} />
+        <Route path="/book" element={<Book />} />
       </Routes>
     </>
   );
