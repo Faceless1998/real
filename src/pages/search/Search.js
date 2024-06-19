998a9e492f02d9febca4bab15ffc4c5ebfcd770c
Faceless1998 <81959592+Faceless1998@users.noreply.github.com>
2024-06-19 14:07:53 +0400

    new

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,52 +1,77 @@
 import React, { useEffect, useState } from "react";
-import "./search.css";
 import axios from "axios";
+import "./search.css";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 
 const Search = () => {
+  const { t } = useTranslation();
+
   const [books, setBooks] = useState([]);
-  const [filteredBooks, setFilteredBooks] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [filterBooks, setFilterBooks] = useState([]);
 
   useEffect(() => {
     axios
       .get("/books.json")
       .then((response) => setBooks(response.data))
-      .catch((error) => console.error("Error Fetching Data ", error));
+      .catch((error) => console.error("Error fetching data: ", error));
   }, []);
+  /*
 
+  {
+                      ? option.phoneNameGeo
+                      : 
+                      ? option.phoneName
+                      : option.phoneNameSp}
+                      */
   useEffect(() => {
     if (searchTerm) {
-      setFilteredBooks(
+      setFilterBooks(
         books.filter((book) =>
-          book.title.includes(searchTerm)
+          localStorage.getItem("lang") === "ge"
+            ? book.titlegeo.toLowerCase().includes(searchTerm.toLowerCase())
+            : localStorage.getItem("lang") === "en"
+            ? book.title.toLowerCase().includes(searchTerm.toLowerCase())
+            : book.titlesp.toLowerCase().includes(searchTerm.toLowerCase()) ||
+              book.author.toLowerCase().includes(searchTerm.toLowerCase()) ||
+              book.id.toString().includes(searchTerm.toString())
         )
       );
     } else {
-      setSearchTerm([]);
+      setFilterBooks([]);
     }
   }, [searchTerm, books]);
 
   const handleChange = (e) => {
     setSearchTerm(e.target.value);
   };
-
+  const handleItemClick = () => {
+    setSearchTerm("");
+    setFilterBooks([]);
+  };
   return (
     <div className="search-container">
       <input
         type="text"
         value={searchTerm}
         onChange={handleChange}
-        placeholder="ძებნა"
+        placeholder={t("search")}
         className="input"
       />
       <ul className="list">
-        {filteredBooks.map((book) =>(
+        {filterBooks.map((book) => (
           <li key={book.id} className="list-item">
-            {book.title} By {book.author}
+            <Link to={`/books/${book.id}`} onClick={handleItemClick}>
+              {localStorage.getItem("lang") === "ge"
+                ? `${book.titlegeo}, ავტორი: ${book.authorgeo}`
+                : localStorage.getItem("lang") === "en"
+                ? `${book.title} By ${book.author}`
+                : `${book.titlesp} Bambino ${book.authorsp}`}
+            </Link>
           </li>
         ))}
-        </ul>
-        
+      </ul>
     </div>
   );
 };
